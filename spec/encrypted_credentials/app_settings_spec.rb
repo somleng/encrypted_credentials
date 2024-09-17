c3dabc80360b9b3671e87636ec204b22829992fd
David Wilkie <dwilkie@gmail.com>
2024-09-17 19:52:16 +0700

    Lazy load APP_MASTER_KEY

--- spec/encrypted_credentials/app_settings_spec.rb
+++ spec/encrypted_credentials/app_settings_spec.rb
@@ -21,6 +21,22 @@ module EncryptedCredentials
       expect(app_settings.credentials.fetch(:secret)).to eq("staging-secret")
     end
 
+    it "handles lazy loading of the APP_MASTER_KEY" do
+      app_settings = build_app_settings(
+        environment: :production,
+        settings_filepath: file_fixture("test_config/app_settings.yml"),
+        encrypted_filepath: file_fixture("test_config/credentials.yml.enc"),
+        key_filepath: file_fixture("test_config/no-key.key")
+      )
+
+      expect(app_settings.env).to eq("production")
+
+      stub_env("APP_MASTER_KEY" => file_fixture("test_config/master.key").read.chomp)
+
+      expect(app_settings[:foo]).to eq("bar")
+      expect(app_settings.fetch(:secret)).to eq("production-secret")
+    end
+
     def build_app_settings(**)
       Class.new(AppSettings).new(config_directory: file_fixture_path.join("test_config"), **)
     end
